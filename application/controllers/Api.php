060fbe5b565325d6515fe4845536033efab10812
Andri Satriawan <satria.citieng@gmail.com>
2022-02-23 13:39:13 +0700

    cek api login

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -24,20 +24,49 @@ class Api extends CI_Controller
 
             $this->session->set_userdata($data);
 
-            echo $this->session->userdata('nama') . "<br>";
-            echo $this->session->userdata('id') . "<br>";
-            echo $this->session->userdata('username') . "<br>";
-            echo $this->session->userdata('member_nama') . "<br>";
-            echo $this->session->userdata('level_user') . "<br>";
+
+            $respons = [
+                'status' => 200,
+                'message' => 'success',
+                'data' => [
+                    'id' => $this->session->userdata('id'),
+                    'nama' => $this->session->userdata('nama'),
+                    'username' => $this->session->userdata('username'),
+                    'member_nama' => $this->session->userdata('member_nama'),
+                    'level_user' => $this->session->userdata('level_user')
+                ]
+            ];
+        } else {
+            $respons = [
+                'status' => 400,
+                'message' => 'gagal, anda tidak dapat mengakses api ini'
+            ];
         }
+
+        echo json_encode($respons);
     }
 
     public function logout()
     {
-        $this->session->unset_userdata('id');
-        $this->session->unset_userdata('nama');
-        $this->session->unset_userdata('username');
-        $this->session->unset_userdata('member_nama');
-        $this->session->unset_userdata('level_user');
+        $key = $this->input->post('key');
+
+        if ($key === 'punyabalaik3') {
+            $this->session->unset_userdata('id');
+            $this->session->unset_userdata('nama');
+            $this->session->unset_userdata('username');
+            $this->session->unset_userdata('member_nama');
+            $this->session->unset_userdata('level_user');
+            $respons = [
+                'status' => 200,
+                'message' => 'success'
+            ];
+        } else {
+            $respons = [
+                'status' => 400,
+                'message' => 'gagal, anda tidak dapat mengakses api ini'
+            ];
+        }
+
+        echo json_encode($respons);
     }
 }
