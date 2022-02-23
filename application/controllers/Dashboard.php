ea82ca95fc224ad1214ee79354a053a5265c334e
Andri Satriawan <satria.citieng@gmail.com>
2022-02-23 13:50:43 +0700

    perbaikan cek login

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Api extends CI_Controller
+class Dashboard extends CI_Controller
 {
     public function index()
     {
-        if (isset($this->session->userdata('id'))) {
+        if ($this->session->userdata('id') != null) {
             $username = $this->session->userdata('username');
             echo "Anda berhasil login dengan username $username";
         } else {
